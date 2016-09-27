ba15a1ff79706d512a6765806fc6c396a59dd39f
bachtran1992 <bachtran1992@gmail.com>
2016-09-27 17:31:40 +0700

    Login

--- login102/ViewController.swift
+++ login102/ViewController.swift
@@ -10,7 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tf_user: UITextField!
+    @IBOutlet weak var tf_password: UITextField!
+    
+            var users = ["cuong":"123","dat":"1","tien":"567" ]
+
+    
+    
     override func viewDidLoad() {
+      
+        
+    
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -20,6 +31,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func action_login(_ sender: AnyObject) {
+        if let password = users[tf_user.text!]
+    {
+        if password == tf_password.text
+        {
+            print("dang nhap thanh cong")
+        }
+        else{
+            print("dang nhap khong thanh cong")
+        }
+        }
+    else
+        {
+            print("tai khoan khong ton tai")
+        }
+    }
 }
+
+
+
+
 
